a73a1dc9fffc3ec4ae0588c2ab711eeb0d8286da
Sean Braithwaite <brapse@gmail.com>
2010-07-31 00:34:38 -0400

    Don't start the cluster unless the workers register

--- reduction/cluster.js
+++ reduction/cluster.js
@@ -1,7 +1,7 @@
 // Start a new cluster on this machine
 
-var           sys = require('sys'),
-             path = require('path');
+var sys = require('sys'),
+   path = require('path');
 
 require.paths.unshift(path.join(__dirname, '..', '..', 'cursed', 'lib'));
 var cursed = require('cursed');
@@ -11,7 +11,6 @@ var       host = process.argv[2] || '127.0.0.1',
    task_module = process.argv[4] || 'tasks';
 
 var task_module_location = __dirname + '/' + task_module;
-cursed.debug(require(task_module_location));
 
 var concurrency = 4;
 var cluster = new(cursed.Cluster)(concurrency, task_module_location, host, port);
@@ -19,4 +18,11 @@ var cluster = new(cursed.Cluster)(concurrency, task_module_location, host, port)
 //// TODO: app configuration
 var router = new(cursed.Connection)('127.0.0.1', 8000);
 
-cluster.register(router);
+cluster.register(router, function(err, result){
+    if (err) {
+        sys.puts("ERROR Starting Cluster: \n" + err);
+        process.exit(0);
+    }
+
+    sys.puts('Cluster started');
+});
--- reduction/worker.js
+++ reduction/worker.js
@@ -1,13 +1,15 @@
 // Child process that actually runs the webserver
 var path = require ('path'),
+     sys = require('sys');
       fs = require('fs');
 
 require.paths.unshift(path.join(__dirname, '..', '..', 'cursed', 'lib'));
 
 var cursed = require('cursed');
 
-var         ip = process.argv[2],
+var       host = process.argv[2],
           port = process.argv[3],
    task_module = process.argv[4];
 
-new(cursed.Worker)(ip, port, task_module).start();
+sys.puts('Starting worker: ' + host + ':' + port); 
+new(cursed.Worker)(host, port, task_module).start();
